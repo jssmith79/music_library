a4e5ff15d292ee3266fef432219f9425cfc5383f
jssmith79 <i.am.hef@gmail.com>
2023-04-16 13:00:15 -0700

    update for day 5

--- src/components/AlbumView.jsx
+++ src/components/AlbumView.jsx
@@ -1,5 +1,4 @@
-// These components will be making separate API calls from the app
-// component to serve specific data about a given album
+
 import { useState, useEffect } from 'react'
 import { useParams} from 'react-router-dom'
 
